f1485c5cd521360a774f11dc5741e8cae61a5957
Luiz Ricardo <luizrgfelipe@gmail.com>
2021-06-16 12:18:39 -0300

    sistema de sepraçao d linhas endo implementado

--- src/organizador.rs
+++ src/organizador.rs
@@ -2,6 +2,7 @@ mod diretorio;
 use std::fs;
 use std::io::BufReader;
 use std::io::Read;
+use std::env;
 
 
 fn ler_arquivo(path:String) -> Vec<String>{
@@ -45,10 +46,55 @@ pub fn let_sped(){
 
 
 
-        let sped =dir_sped.split("/"); 
+        let mut sped = Vec::new();
+        //veirificando sistema operacional
+        if env::consts::OS == "windows"{
 
-        println!("{}",dir_sped);
+            sped = dir_sped.split("\\").collect();
+            
+        }else{
+            
+            sped = dir_sped.split("/").collect();
+
+        }
+
+        let sped = sped[sped.len()-1]; //nome do sped
+
+        //abrindo o sped.txt
+        let linhas_sped = ler_arquivo(dir_sped);
+        //analisando linha por linha do sped
+        for linha in linhas_sped{
+
+            //abrindo xmls
+            for dir_xml in &dirs_xmls{
+
+                if linha.contains(dir_xml){
+
+                    organizar(dir_xml.to_string(), linha)
+
+
+                }
+
+            }
+
+        }
+
+
+
+
 
     }
+
+}
+
+fn pegar_linhas_seped_importantes(){
+
+    
+
+}
+
+fn organizar(dir_xml:String,line_sped:String){
+
+
 
 }
